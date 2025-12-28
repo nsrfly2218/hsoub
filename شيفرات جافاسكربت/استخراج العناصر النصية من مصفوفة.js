7c44b16043eded4a14ce76db83cc1c2c0878fe4d
nsrfly2218 <165476039+nsrfly2218@users.noreply.github.com>
2025-12-28 04:24:40 +0300

    Delete شيفرات جافاسكربت directory

--- شيفرات جافاسكربت/استخراج العناصر النصية من مصفوفة.js
+++
@@ -1,9 +0,0 @@
-let data = ["Anas", 21, 23, true, "Omar", 19, false];
-for (let item of data) {
-  if (typeof item === "string") {
-    console.log(item);
-  }
-}
-// Output:
-// Anas
-// Omar
--- شيفرات جافاسكربت/جمع عناصر المصفوفة.js
+++
@@ -1,9 +0,0 @@
-let prices = [100, 250, 50, 89];
-let totalPrice = 0;
-for (let price of prices) {
-  totalPrice += price;
-}
-console.log(totalPrice);
-
-// Output:
-// 489
